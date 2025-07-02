c85640241543bde436fd9911e3bcf34ac50753e8
dihaxn <ihanlaknuka@gmail.com>
2025-07-02 14:27:36 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import About from "./pages/About.jsx";
 import Home from "./pages/Home.jsx";
+import Login from "./components/Login.jsx";
+import RegisterForm from "./components/RegisterForm.jsx";
 
 function App() {
 
   return (
     <>
         <Navbar />
-        <Home/>
+        <RegisterForm/>
         <Footer/>
     </>
   )
